47f9f30f82fbf27a8e7e8172473acd80d379df24
D.A Huemaen <d.ahuemaen@DAs-MacBook-Air.fios-router.home>
2019-11-15 00:08:14 -0500

    Started the Basics of JS

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,22 @@
+
+// JavaScript Data Types
+var firstName = "John";
+console.log(firstName)
+
+// strings are ant sequence of characters-- text
+var lastName = "Smith";
+
+// floating point numbers for decimals and integers
+var age = 28;
+
+// boolean are simply true or false
+var fullAge = true;
+console.log(fullAge)
+
+// undefined means no values has been give it yet
+var job; 
+console.log(job)
+
+job = 'teachers'
+console.log(job);
+
