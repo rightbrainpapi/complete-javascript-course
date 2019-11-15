21f691422ef5e60d164099b318eda0882d384a69
D.A Huemaen <d.ahuemaen@DAs-MacBook-Air.fios-router.home>
2019-11-15 07:57:30 -0500

    Variable Mutation & Type Coercion

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,22 +1,51 @@
+/////////////////////////////////
+///////Variable & Data Types/////
+/////////////////////////////////
 
-// JavaScript Data Types
-var firstName = "John";
-console.log(firstName)
+// var firstName = "John";
+// console.log(firstName)
 
-// strings are ant sequence of characters-- text
-var lastName = "Smith";
+// // strings are ant sequence of characters-- text
+// var lastName = "Smith";
 
-// floating point numbers for decimals and integers
-var age = 28;
+// // floating point numbers for decimals and integers
+// var age = 28;
 
-// boolean are simply true or false
-var fullAge = true;
-console.log(fullAge)
+// // boolean are simply true or false
+// var fullAge = true;
+// console.log(fullAge)
 
-// undefined means no values has been give it yet
-var job; 
-console.log(job)
+// // undefined means no values has been give it yet
+// var job; 
+// console.log(job)
 
-job = 'teachers'
-console.log(job);
+// job = 'teachers'
+// console.log(job);
 
+//////////////////////////////////
+//Variable Mutation & Coercion////
+//////////////////////////////////
+var firstName = "John"
+var age =28;
+
+
+//Type Coercion
+console.log(firstName + " "+ age)
+
+
+var job, isMarried
+job = "teacher";
+isMarried = false;
+
+console.log(firstName + " is a " + age + " year old " + job + ". Is he married? " + isMarried)
+
+
+// Variable Mutation - Changing the value of a variable
+
+age = "twenty eight";
+job = "driver";
+
+alert(firstName + " is a " + age + " year old " + job + ". Is he married? " + isMarried)
+
+var lastName = prompt("What is his last Name?");
+console.log(firstName + " " + lastName)
