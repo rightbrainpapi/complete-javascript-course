1a321d2ecdf94745c2daadb4cdc72966583f06eb
D.A Huemaen <d.ahuemaen@DAs-MacBook-Air.fios-router.home>
2019-11-15 08:19:35 -0500

    Bssic Javascript Operators

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -25,28 +25,63 @@
 //////////////////////////////////
 //Variable Mutation & Coercion////
 //////////////////////////////////
-var firstName = "John"
-var age =28;
+// var firstName = "John"
+// var age =28;
 
 
-//Type Coercion - Process of converting one value type to another
-console.log(firstName + " "+ age)
+// //Type Coercion - Process of converting one value type to another
+// console.log(firstName + " "+ age)
 
 
-var job, isMarried
-job = "teacher";
-isMarried = false;
+// var job, isMarried
+// job = "teacher";
+// isMarried = false;
 
-console.log(firstName + " is a " + age + " year old " + job + ". Is he married? " + isMarried)
+// console.log(firstName + " is a " + age + " year old " + job + ". Is he married? " + isMarried)
 
 
-// Variable Mutation - Changing the value of a variable
-age = "twenty eight";
-job = "driver";
+// // Variable Mutation - Changing the value of a variable
+// age = "twenty eight";
+// job = "driver";
 
-// Using Alert
-alert(firstName + " is a " + age + " year old " + job + ". Is he married? " + isMarried)
+// // Using Alert
+// alert(firstName + " is a " + age + " year old " + job + ". Is he married? " + isMarried)
 
-//Using Promt 
-var lastName = prompt("What is his last name?");
-console.log(firstName + " " + lastName)
+// //Using Promt 
+// var lastName = prompt("What is his last name?");
+// console.log(firstName + " " + lastName)
+
+
+
+//////////////////////////////////
+////Basic Javascript Operators////
+//////////////////////////////////
+
+var now, yearJohn, yearMark;
+var now = 2019
+var ageJohn = 29
+var ageMark = 33
+
+// Math Operators 
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+console.log(yearJohn);
+console.log(yearMark);
+
+
+console.log(now + 2)
+console.log(now * 2)
+console.log(now / 10)
+
+// Logical Operators
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+// typeof Operator
+console.log(typeof johnOlder);
+console.log(typeof "Mark is older than John");
+console.log(typeof ageJohn);
+console.log(typeof x)
+
+
+
